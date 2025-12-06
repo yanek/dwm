8b27d7476ac1023a755ba15647afb94f4438d1ff
Noé Ksiazek <noe.ksiazek@pm.me>
2025-12-06 12:05:18 +0100

    flake: Init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A custom build of dwm";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    architectures = ["x86_64-linux" "aarch64-linux"];
+  in {
+    overlays.default = final: prev: {
+      dwm = prev.dwm.overrideAttrs (oldAttrs: {
+        src = self;
+      });
+    };
+
+    packages = nixpkgs.lib.genAttrs architectures (system: {
+      default =
+        (import nixpkgs {
+          inherit system;
+          overlays = [self.overlays.default];
+        }).dwm;
+    });
+
+    devShells = nixpkgs.lib.genAttrs architectures (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [xorg.libX11 xorg.libXft xorg.libXinerama gcc bear];
+      };
+    });
+  };
+}
